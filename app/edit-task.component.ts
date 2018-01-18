f95613dcd2e578008957d38abfe61351a4069a1f
Amy Churchwell and Luke Bertram <luke@>
2018-01-18 11:37:59 -0800

    integrate edit-task component

--- app/edit-task.component.ts
+++ app/edit-task.component.ts
@@ -5,23 +5,28 @@ import { Task } from './task.model';
   selector: 'edit-task',
   template: `
   <div>
-    <div *ngIf="selectedTask">
-      <h3>{{selectedTask.description}}</h3>
-      <p>Task Complete? {{selectedTask.done}}</p>
+    <div *ngIf="childSelectedTask">
+      <h3>{{childSelectedTask.description}}</h3>
+      <p>Task Complete? {{childSelectedTask.done}}</p>
       <hr>
       <h3>Edit Task</h3>
       <label>Enter Task Description:</label><br />
-      <input type="text" [(ngModel)]="selectedTask.description">
+      <input type="text" [(ngModel)]="childSelectedTask.description">
       <label>Enter Task Priority (1-3):</label><br/>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)<br>
-      <button (click)="finishedEditing()">Done</button>
+      <input type="radio" [(ngModel)]="childSelectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio" [(ngModel)]="childSelectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="childSelectedTask.priority" [value]="3">3 (High Priority)<br>
+      <button (click)="doneButtonClicked()">Done</button>
     </div>
   </div>
   `
 })
 
 export class EditTaskComponent {
-  
+  @Input() childSelectedTask: Task;
+  @Output() doneButtonClickedSender = new EventEmitter();
+
+  doneButtonClicked() {
+    this.doneButtonClickedSender.emit();
+  }
 }
